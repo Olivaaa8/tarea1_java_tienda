75b40d7c8657a7f53a586110ce972dabf2b93814
Oscar Oliva <yahiroliva97@gmail.com>
2025-08-01 00:39:41 -0600

    feat: Logica ver productos, agregar al carrito y comprar

--- src/Main.java
+++ src/Main.java
@@ -22,12 +22,33 @@ public class Main {
             switch (opcion) {
                 case 1:
                     // Mostrar productos
+                    System.out.println("Productos disponibles:");
+                    for (int i = 0; i < productos.length; i++) {
+                        System.out.println((i + 1) + ". " + productos[i] + " - $" + precios[i]);
+                    }
                     break;
                 case 2:
                     // Agregar al carrito
+                    System.out.println("Ingrese el numero del producto a agregar al carrito:");
+                    int productoSeleccionado = o.nextInt() - 1;
+                    if (productoSeleccionado >= 0 && productoSeleccionado < productos.length) {
+                        carrito[productoSeleccionado] = productos[productoSeleccionado];
+                        System.out.println(productos[productoSeleccionado] + " ha sido agregado al carrito.");
+                    } else {
+                        System.out.println("Producto no valido.");
+                    }
                     break;
                 case 3:
                     // Calcular total a pagar
+                    double total = 0.0;
+                    System.out.println("Productos en el carrito:");
+                    for (int i = 0; i < carrito.length; i++) {
+                        if (carrito[i] != null) {
+                            System.out.println(carrito[i] + " - $" + precios[i]);
+                            total += precios[i];
+                            System.out.println("Total a pagar: $" + total);
+                        }
+                    }
                     break;
                 case 4:
                     // Salir
