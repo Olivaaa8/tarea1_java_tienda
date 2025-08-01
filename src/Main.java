8a24f80969e88efe6694b7b307199892438cceb5
Oscar Oliva <yahiroliva97@gmail.com>
2025-08-01 00:03:17 -0600

    feat: Estrutura base

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Arreglos de productos, precios, carrito
+        String[] productos = {"Laptop", "Audifonos", "Iphone", "Tablet"};
+        double[] precios = {1000.0, 200.0, 800.0, 300.0};
+        String[] carrito = new String[productos.length];
+        Scanner o = new Scanner(System.in);
+    }
+}
