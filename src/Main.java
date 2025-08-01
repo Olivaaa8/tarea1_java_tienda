ab3c3d44ce74be30bb49a04aed7fdf415797dd49
Oscar Oliva <yahiroliva97@gmail.com>
2025-08-01 00:25:36 -0600

    feat: Logica de menu

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         // Arreglos de productos, precios, carrito
@@ -9,5 +7,35 @@ public class Main {
         double[] precios = {1000.0, 200.0, 800.0, 300.0};
         String[] carrito = new String[productos.length];
         Scanner o = new Scanner(System.in);
+
+        int opcion;
+
+        // Menu de opciones
+        do {
+            System.out.println("Seleccione una opcion:");
+            System.out.println("1. Ver productos");
+            System.out.println("2. Agregar al carrito");
+            System.out.println("3. calcular total a pagar");
+            System.out.println("4. Salir");
+            opcion = o.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    // Mostrar productos
+                    break;
+                case 2:
+                    // Agregar al carrito
+                    break;
+                case 3:
+                    // Calcular total a pagar
+                    break;
+                case 4:
+                    // Salir
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    System.out.println("Opcion no valida.");
+            }
+        } while (opcion != 4);
     }
 }
